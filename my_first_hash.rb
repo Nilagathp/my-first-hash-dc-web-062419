54bc7f1f85d50c0333aecc8ae59281a0a3a8c725
Hannah N <nilagathp@gmail.com>
2019-06-14 17:10:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,21 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+
   your_hash = {"name" => "Sophie"}
 end
 
+
 def shipping_manifest
-  the_manifest = {"Whale bone corsets" => 5, "Porcelain vases" => 2, "Oil paintings" => 3 }
-end 
+  # set a variable called `the_manifest`, equal to a hash
+  # fill that hash with key/value pairs that describe the following information: 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+
+  the_manifest = {
+      "whale bone corsets" => 5, 
+      "porcelain vases" => 2, 
+      "oil paintings" => 3
+    }
+end
 
 def retrieval
   shipping_manifest = {
@@ -13,7 +23,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                   shipping_manifest["oil paintings"]
+
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -22,8 +34,14 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 
-                     shipping_manifest["muskets"] =2 
-                         shipping_manifest["gun powder"] = 4
-                             shipping_manifest
+  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
+
+
+  # return the shipping_manifest hash below
+  shipping_manifest
 end
